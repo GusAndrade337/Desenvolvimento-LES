6c1ac5d130021612ccf099203a5668ad4c0100b1
GusAndrade337 <99684321+GusAndrade337@users.noreply.github.com>
2023-11-10 17:22:49 -0300

    Node Js

--- Node-Les/index.js
+++ Node-Les/index.js
@@ -3,61 +3,49 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000; // You can change this port if needed
+const port = 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public')); // Create a "public" folder for your static assets
+app.use(express.static('public')); 
 app.use(bodyParser.json());
 
-// index.js
 app.get('/', async (req, res) => {
     try {
-      // Get the "page" parameter from the URL or default to page 1
       const page = parseInt(req.query.page) || 1;
   
-      // Make an API request to your backend (localhost:9000) with the "page" parameter
       const response = await axios.get(`http://localhost:8080/Chamados?size=6&page=${page}`);
   
-      // Check if the response contains an array of "content" Chamados
       if (Array.isArray(response.data.content)) {
-        // Extract the "content" Chamados and their "name" property
         const contentChamados = response.data.content;
   
-        // Calculate the total number of pages based on your data (e.g., response.data.totalPages)
         const totalPages = response.data.totalPages;
   
-        // Render the EJS template with the content Chamados and pagination data
         res.render('index', { contentChamados, totalPages });
       } else {
-        console.error('Invalid data format:', response.data);
+        console.error('Formato de dados invalidos:', response.data);
         res.status(500).send('Invalid Data Format');
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Erro ao conversar com o backend:', error);
       res.status(500).send('Internal Server Error');
     }
   });
 
   app.get('/chamado/:id', async (req, res) => {
     try {
-      const ChamadoId = req.params.id; // Get the Chamado ID from the URL
-  
-      // Make an API request to your backend with the Chamado ID
+      const ChamadoId = req.params.id; 
       const response = await axios.get(`http://localhost:8080/Chamados/${ChamadoId}`);
   
-      // Check if the response contains the Chamado details
       if (response.data) {
-        // Extract the Chamado details
         const chamado = response.data;
   
-        // Render the Chamado details EJS template
         res.render('detail', { chamado });
       } else {
-        console.error('Chamado not found or invalid data format');
+        console.error('Chamado not found');
         res.status(404).send('chamado not found');
       }
     } catch (error) {
-      console.error('Error fetching chamado details:', error);
+      console.error('Error ao buscar detalhes do chamado:', error);
       res.status(500).send('Internal Server Error');
     }
   });
@@ -67,7 +55,7 @@ app.get('/', async (req, res) => {
         res.send('Feature ainda não implementada');
 
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Erro ao conversar com o backend', error);
       res.status(500).send('Internal Server Error');
     }
   }); 
@@ -77,7 +65,7 @@ app.get('/', async (req, res) => {
         res.send('Feature ainda não implementada');
 
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Erro ao conversar com o backend', error);
       res.status(500).send('Internal Server Error');
     }
   }); 
@@ -85,13 +73,11 @@ app.get('/', async (req, res) => {
   app.get('/criarChamado', (req, res) => {
     res.render('criarChamado');
   });
-  // Dummy in-memory data
   let Chamados = [];
 
   app.post('/Chamado', (req, res) => {
     const newChamado = req.body;
   
-    // Assign a unique ID to the new Chamado (you may want to use a library for this)
     newChamado.id = Chamados.length + 1;
   
     Chamados.push(newChamado);
